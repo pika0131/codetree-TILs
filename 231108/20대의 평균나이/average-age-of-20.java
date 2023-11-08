cc43f4074999b7978a6740a2b9ca77197887ebc5
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-08 11:42:13 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231108/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static double ave = 0;
+	static int cnt = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		while (true) {
+			int a = Integer.parseInt(br.readLine());
+			if (a > 29) {
+				break;
+			}
+			ave += a;
+			cnt++;
+		}
+		System.out.printf("%.2f", ave / cnt);
+	}
+}
