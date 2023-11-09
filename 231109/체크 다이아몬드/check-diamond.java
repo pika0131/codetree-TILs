f02991b3c97330cbbdeebc2c9f25fa1f30c7be16
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-09 13:20:22 +0900

    Title: 체크 다이아몬드, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231109/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= 2 * n - 1; i++) {
+			for (int j = 1; j <= 2 * n - 1; j++) {
+				if (i <= n) {
+					if (j >= n - i + 1 && j <= n + i - 1) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				} else {
+					if (j >= i - n + 1 && j <= 3 * n - i - 1) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+}
