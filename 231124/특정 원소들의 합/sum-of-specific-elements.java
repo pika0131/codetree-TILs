f6a3d59edf99a2db1b3658fc624febe40d4244dc
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-24 20:28:39 +0900

    Title: 특정 원소들의 합, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231124/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] list = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                list[i][j] = sc.nextInt();
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += list[i][j];
+            }
+        }
+        System.out.print(sum);
+    }
+}
