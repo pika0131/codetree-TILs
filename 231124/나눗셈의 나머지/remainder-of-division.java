13afb57845ed4bdd7e07eda542b3e9822cfa1727
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-24 20:05:17 +0900

    Title: 나눗셈의 나머지, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

--- 231124/나눗셈의 나머지/remainder-of-division.java
+++ 231124/나눗셈의 나머지/remainder-of-division.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] list = new int[1000];
-        int[] cnt = new int[9];
+        int[] cnt = new int[1000];
         int a = sc.nextInt();
         int b = sc.nextInt();
         int count = 0;
@@ -18,7 +18,7 @@ public class Main {
             count++;
         }
         int sum = 0;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < cnt.length; i++) {
             sum += cnt[i] * cnt[i];
         }
         System.out.println(sum);
