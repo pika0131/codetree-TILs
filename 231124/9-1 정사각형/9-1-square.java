37732961c331dc6e1980ae64f2eb7512cc2c1aca
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-24 15:44:03 +0900

    Title: 9-1 정사각형, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231124/9-1 정사각형/9-1-square.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
+        int cnt = 9;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cnt == 1) {
+                    sb.append(cnt + " ");
+                    cnt = 9;
+                    continue;
+                }
+                sb.append(cnt + " ");
+                cnt--;
+
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
