ceadccfc96d4ab2a33b2bcc511f3c3e9a9fc87dd
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-24 16:55:24 +0900

    Title: 배열에서 원소의 특징, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231124/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
+++ 231124/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] list = new int[10];
-        for (int i = 0; i < 10; i++) {
-            list[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < list.length; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i : list) {
+        for (int i = 0; i < 10; i++) {
             if (list[i] % 3 == 0) {
-                System.out.print(list[i - 1]);
+                System.out.print(list[i]);
                 break;
             }
         }
