82d4d1e429528522ed853ce4fe9d5b2837e9b111
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-26 00:23:43 +0900

    Title: 가장 짧은 문자열, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 231126/가장 짧은 문자열/shortest-string.java
+++ 231126/가장 짧은 문자열/shortest-string.java
@@ -4,12 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int MIN = Integer.MAX_VALUE;
+        int MAX = Integer.MIN_VALUE;
         for (int i = 0; i < 3; i++) {
             String str = sc.nextLine();
             if (str.length() < MIN) {
                 MIN = str.length();
             }
+            if (str.length() > MAX) {
+                MAX = str.length();
+            }
         }
-        System.out.println(MIN);
+        System.out.println(MAX - MIN);
     }
 }
