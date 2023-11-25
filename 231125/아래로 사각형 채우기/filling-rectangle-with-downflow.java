f89c1d0567846d6cfa6bc4961dddc4f13d60646b
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-25 17:34:17 +0900

    Title: 아래로 사각형 채우기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231125/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int n = Integer.parseInt(br.readLine());
+        int[][] list = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    list[i][j] = i + 1;
+                } else {
+                    list[i][j] = list[i][j - 1] + n;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(list[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
