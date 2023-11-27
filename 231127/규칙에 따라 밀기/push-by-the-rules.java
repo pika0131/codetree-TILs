6f919b92d975f8acd1a62681a9a022a3be4d47ed
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-27 22:23:37 +0900

    Title: 규칙에 따라 밀기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231127/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        String str = br.readLine();
+        String command = br.readLine();
+        ;
+        Deque<Character> dq = new ArrayDeque<>();
+        for (int i = 0; i < str.length(); i++) {
+            dq.add(str.charAt(i));
+        }
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'L') {
+                dq.addLast(dq.pollFirst());
+            } else {
+                dq.addFirst(dq.pollLast());
+            }
+        }
+        for (int i : dq) {
+            System.out.print(Character.toString(i) + "");
+        }
+    }
+}
