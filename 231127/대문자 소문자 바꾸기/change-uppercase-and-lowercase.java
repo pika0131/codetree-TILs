df8cf65ab6b9ba624e47c22948a7fcd40f887ebd
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-27 22:39:56 +0900

    Title: 대문자 소문자 바꾸기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231127/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        System.out.println(sb);
+    }
+}
