deaef92595ae07722f1543ebb40717697c6d8834
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-27 22:17:45 +0900

    Title: e 제거하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 231127/e 제거하기/e-to-remove.java
+++ 231127/e 제거하기/e-to-remove.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
         String str = br.readLine();
-        String str1 = str.replace("e", "");
-        System.out.println(str1);
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        int idx = str.indexOf("e");
+        sb.deleteCharAt(idx);
+
+        System.out.println(sb);
     }
 }
