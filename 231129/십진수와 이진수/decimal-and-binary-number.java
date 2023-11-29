bac14b1a795501898269ece88c26d6a18662fe95
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-29 23:30:15 +0900

    Title: 십진수와 이진수, Time: 143ms, Memory: 9MB, Status: Passed - CodeTree

--- 231129/십진수와 이진수/decimal-and-binary-number.java
+++ 231129/십진수와 이진수/decimal-and-binary-number.java
@@ -1,12 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int a = Integer.parseInt(str, 2);
-        a *= 17;
-        String str2 = Integer.toBinaryString(a);
-        System.out.println(str2);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        BigInteger bigInteger = new BigInteger(str, 2);
+        bigInteger = bigInteger.multiply(BigInteger.valueOf(17));
+        String result = bigInteger.toString(2);
+        System.out.println(result);
     }
 }
