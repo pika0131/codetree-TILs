458f766c7bee5cfd08f28df3a444bffb52bd99fb
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-29 23:35:08 +0900

    Title: 알파벳 회전초밥, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231129/알파벳 회전초밥/alphabet-rotating-sushi.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String str2 = br.readLine();
+        System.out.println(str2.length());
+    }
+}
