4b8e335f211eb3810a4ae499d4903a4aa00e6a8b
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-11-29 18:14:51 +0900

    Title: 함수를 이용한 소수 판별, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231129/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isprime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            if (isprime(i)) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
