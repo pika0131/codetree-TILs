ce2ff3b04610eaa187f4c78f9187ca87c867fd49
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-12-02 21:42:34 +0900

    Title: 2021년 날짜의 유무, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231202/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static boolean isValid(int M, int D) {
+        if (M == 2) {
+            if (D > 28) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (D > 31) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        int cnt = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        if (isValid(M, D)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
