3bb2684461840c5436be78a3c36a297d3015c5e6
pika0131 <129724220+pika0131@users.noreply.github.com>
2023-12-02 21:37:48 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 231202/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 231202/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,46 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class Main {
-    static int n1;
-    static int n2;
+public class Main {    
+    
+   static int n1, n2;
     static int[] a = new int[100];
     static int[] b = new int[100];
-
-    public static boolean cal(int n) {
-        for (int i = 0; i < n2; i++)
-            if (a[i + n] != b[i])
+    
+    public static boolean check(int n) {
+        for(int i = 0; i < n2; i++)
+            if(a[i + n] != b[i])
                 return false;
-
+    
         return true;
     }
-
     public static boolean isSubsequence() {
-        for (int i = 0; i <= n1 - n2; i++)
-            if (cal(i))
+        for(int i = 0; i <= n1 - n2; i++)
+            if(check(i))
                 return true;
-
+        
         return false;
     }
 
-    public static void main(String[] args) throws IOException {
-        int cnt = 0;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n1 = Integer.parseInt(st.nextToken());
-        int n2 = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n1; i++)
-            a[i] = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n2; i++)
-            b[i] = Integer.parseInt(st.nextToken());
-        if (isSubsequence()) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        for(int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+        
+        for(int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+
+        
+        if(isSubsequence())
+            System.out.print("Yes");
+        else
+            System.out.print("No");
     }
 }
