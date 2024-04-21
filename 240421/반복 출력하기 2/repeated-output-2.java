310c8fd32582e8ad20ad58ecf9afaf23ddb33748
pika0131 <129724220+pika0131@users.noreply.github.com>
2024-04-21 23:48:29 +0900

    Title: 반복 출력하기 2, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void printStar(int n){
+        if(n == 0){
+            return;
+        }
+        printStar(n - 1);
+        System.out.println("HelloWorld");
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printStar(n);
+    }
+}
